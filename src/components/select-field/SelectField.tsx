be2272435e51ced1d885e6792dc8df1791c41604
Victor Desyatkin <vitek1985@mail.ru>
2021-03-28 01:19:05 +0500

    add useMemo for memoizedOptionsBody, memoizedItems, memoizedPreparedItems

--- src/components/select-field/SelectField.tsx
+++ src/components/select-field/SelectField.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
+import classnames from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTimesCircle,
+  faChevronCircleDown,
+} from '@fortawesome/free-solid-svg-icons';
 
 import './select-field.scss';
 
@@ -11,11 +17,12 @@ type SelectFieldOptionType = {
 
 type SelectFieldType = {
   name?: string;
-  value?: string | number;
+  value?: string | number | readonly string[];
   ariaLabel?: string;
   options?: SelectFieldOptionType[];
   isMultiple?: boolean;
   isDisabled?: boolean;
+  placeholder?: string | number;
 };
 
 const SelectField: React.FC<SelectFieldType> = ({
@@ -25,35 +32,171 @@ const SelectField: React.FC<SelectFieldType> = ({
   options,
   isMultiple,
   isDisabled,
+  placeholder,
 }) => {
+  //         className="select-field__option"
+  let isReadyMultiple = isMultiple;
+  if (!isReadyMultiple && Array.isArray(value) && value.length) {
+    isReadyMultiple = true;
+  }
+  const prepareItems = (items?: string | number | readonly string[]) => {
+    let readyItems;
+    console.log('prepareItems : ');
+    if (items) {
+      readyItems = [];
+      if (Array.isArray(items)) {
+        readyItems = Array.from(items);
+      } else {
+        readyItems.push(String(items));
+      }
+    }
+    return readyItems;
+  };
+  const [selectedValue, setSelectedValue] = useState(value);
+  const [isOpened, setIsOpened] = useState(false);
   const renderOption = ({
     value: valueOption,
     content,
     isDisabled: isDisableOption,
     id,
   }: SelectFieldOptionType) => {
-    console.log('id : ', id);
     return (
       <option value={valueOption} disabled={isDisableOption} key={id}>
         {content}
       </option>
     );
   };
-  const renderOptions = (optionsForRender: SelectFieldOptionType[]) => {
-    return optionsForRender.map(renderOption);
+  const renderOptions = (optionsForRender?: SelectFieldOptionType[]) => {
+    if (optionsForRender) {
+      return optionsForRender.map(renderOption);
+    }
+    return optionsForRender;
   };
-  console.log('options : ', options);
+  const handleSelectChange: React.ChangeEventHandler<HTMLSelectElement> = (
+    element
+  ) => {
+    console.log('element : ', element);
+  };
+  const onClickItemControl = (index: number) => {
+    let copySelectedValue: string | number | string[] | undefined;
+    if (Array.isArray(selectedValue)) {
+      copySelectedValue = Array.from(selectedValue);
+      copySelectedValue.splice(index, 1);
+      if (!copySelectedValue.length) {
+        copySelectedValue = undefined;
+      }
+    } else {
+      copySelectedValue = undefined;
+    }
+    setSelectedValue(copySelectedValue);
+  };
+  const renderItem = (item: string, index: number) => {
+    console.log('renderItem : ');
+    return (
+      <li className="select-field__item" key={`select-field__item_${index}`}>
+        <span className="select-field__item-content">{item}</span>
+        <button
+          className="select-field__item-control"
+          type="button"
+          onClick={() => onClickItemControl(index)}
+        >
+          <FontAwesomeIcon icon={faTimesCircle} />
+        </button>
+      </li>
+    );
+  };
+  const renderItems = (items?: readonly string[]) => {
+    console.log('renderItems : ');
+    return items?.map(renderItem);
+  };
+  const memoizedPreparedItems = useMemo(() => prepareItems(selectedValue), [
+    selectedValue,
+  ]);
+  const memoizedItems = useMemo(() => renderItems(memoizedPreparedItems), [
+    memoizedPreparedItems,
+  ]);
+  const renderPlaceholder = (placeholderForRender?: string | number) => {
+    if (placeholderForRender) {
+      return (
+        <span className="select-field__placeholder">
+          {placeholderForRender}
+        </span>
+      );
+    }
+    return placeholderForRender;
+  };
+  const onClickButtonControl = () => {
+    setIsOpened(!isOpened);
+  };
+  const renderOptionBody = (optionForRender?: SelectFieldOptionType) => {
+    console.log('renderOptionBody : ');
+    if (optionForRender) {
+      const {
+        value: valueOption,
+        content,
+        isDisabled: isDisableOption,
+        id,
+      } = optionForRender;
+      const className = 'select-field__option';
+      const isSelected = memoizedPreparedItems?.some(
+        (item) => String(item) === String(valueOption)
+      );
+      return (
+        <button
+          className={classnames(className, {
+            [`${className}__selected`]: isSelected,
+          })}
+          type="button"
+          disabled={isDisableOption}
+        >
+          {content}
+        </button>
+      );
+    }
+    return optionForRender;
+  };
+  const renderOptionsBody = (optionsForRender?: SelectFieldOptionType[]) => {
+    console.log('renderOptionsBody : ');
+    if (optionsForRender) {
+      return optionsForRender.map(renderOptionBody);
+    }
+    return optionsForRender;
+  };
+  const memoizedOptionsBody = useMemo(() => renderOptionsBody(options), [
+    options,
+  ]);
+  console.log('selectedValue : ', selectedValue);
+  const className = 'select-field';
   return (
-    <select
-      name={name}
-      value={value}
-      disabled={isDisabled}
-      aria-label={ariaLabel}
-      multiple={isMultiple}
-      className="select-field"
+    <article
+      className={classnames(className, { [`${className}_opened`]: isOpened })}
     >
-      {options && renderOptions(options)}
-    </select>
+      <div className="select-field__head">
+        {selectedValue ? (
+          <ul className="select-field__items">{memoizedItems}</ul>
+        ) : (
+          renderPlaceholder(placeholder)
+        )}
+        <button
+          className="select-field__control"
+          type="button"
+          onClick={onClickButtonControl}
+        >
+          <FontAwesomeIcon icon={faChevronCircleDown} />
+        </button>
+      </div>
+      <div className="select-field__body">{memoizedOptionsBody}</div>
+      <select
+        name={name}
+        defaultValue={selectedValue}
+        disabled={isDisabled}
+        aria-label={ariaLabel}
+        multiple={isReadyMultiple}
+        hidden
+      >
+        {renderOptions(options)}
+      </select>
+    </article>
   );
 };
 
