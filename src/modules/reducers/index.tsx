9e47aa6a7c5d515f091c9a86dc09ac4af2ef232f
Victor Desyatkin <vitek1985@mail.ru>
2021-04-10 17:57:14 +0500

    add root reducer

--- src/modules/reducers/index.tsx
+++ src/modules/reducers/index.tsx
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 
 import leagueReducer from './leagues';
 import teamsReducer from './teams';
-import root from './root';
 import { initialStateProps } from '../types';
 
 const reducer = combineReducers({
   leagues: leagueReducer,
   teams: teamsReducer,
-  root,
 });
 
 export type { initialStateProps };
